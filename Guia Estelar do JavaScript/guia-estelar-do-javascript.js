65409f1995d3b4ffa3a69cb322431005b5a4126f
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2022-04-18 18:31:47 -0400

    add: guia-estelar-do-javascript.js, guia-estelar-do-javascript.md, page.html (FIRST COMMIT)

---
+++ Guia Estelar do JavaScript/guia-estelar-do-javascript.js
@@ -0,0 +1,24 @@
+// String
+
+console.log('string type data')
+console.log('I can also write using simbols like @#$%')
+console.log("isn't complex. It's very simple")
+console.log(`now 
+i'm
+using
+template literals.  
+So i can calculate, like {120 + 514 - 4.336 * 223}
+I just have to put a "$" before, like this:
+${120 + 514 - 4.336 * 223}`)
+
+// Number
+
+console.log(33)
+console.log(-33)
+console.log(12.4)
+console.log(Infinity)
+console.log(NaN)
+console.log(12.5 * 2 * (11 * 10))
+console.log(12.5 * 2 * '(11 * 10)') // returns "NaN"
+console.log(42 === Infinity) // returns "false"
+console.log(21 * 2 === 42) // returns "true"
---
+++ Guia Estelar do JavaScript/guia-estelar-do-javascript.md
@@ -0,0 +1,22 @@
+# DATA TYPES
+
+## String
+
+- É uma cadeia de caracteres. Podem ser números, letras, símbolos, espaços, podem ter pontuação, acentuação.
+
+"" // aspas duplas
+'' // aspas simples
+`` // template literals ou template strings (mais funcionalidades)
+
+- Tomar cuidado com as strings que tiverem algum dos elementos acima, pois podem dar erro.
+
+- As crases (template literals) permitem quebra de linhas e interpolação (cálculo utilizando "${cálculo aqui}")
+
+## Number
+
+- Basicamente números de vários tipos.
+
+33 ou -33 // inteiros
+12.4 // reais
+NaN // not a number
+Infinity // infinito
